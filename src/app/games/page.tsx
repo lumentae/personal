68f0d0fba170b9619d755783d026cf83754295cb
lumentae <66220444+lumentae@users.noreply.github.com>
2025-06-30 08:38:12 +0000

    Add api

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,7 +1,27 @@
 import {List} from "@/components/List";
 import {Game} from "@/components/Game";
+import { useEffect, useState } from "react";
+
+interface GameEntry {
+  title: string,
+  activePlayers: number,
+  visits: number,
+  likes: number,
+  imageUrl: string,
+  gameId: number
+}
 
 export default function Games() {
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+      fetch("https://fynn93.dev/api")
+        .then((res) => res.json())
+        .then((data) => setGames(data));
+
+  })
+  const gameList = games.map(game => 
+    <Game title={game.title} activePlayers={game.activePlayers} visits={game.visits} likes={game.likes} imageUrl={game.imageUrl} gameId={game.gameId}
+  )
   return (
     <List>
       <Game title={"Scary Cat [HORROR]"} activePlayers={234} visits={43234344} likes={120132} imageUrl={"/Scary Cat.png"} gameId={18663227290}></Game>
