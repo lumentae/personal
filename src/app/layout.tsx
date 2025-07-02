86e8e65e1eb2a122a4c0443910d8f66eed0b79aa
lumentae <66220444+lumentae@users.noreply.github.com>
2025-07-02 12:20:23 +0000

    Add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 import "./globals.css";
 
 export default function RootLayout({
@@ -14,6 +15,7 @@ export default function RootLayout({
       <body>
         <Header/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
